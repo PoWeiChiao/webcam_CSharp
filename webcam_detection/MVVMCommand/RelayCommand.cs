9e6e6e1d753d42ee12b9f913c7f9c1e82f665776
PoWeiChiao <pwchiao@hotmail.com>
2021-06-12 01:13:36 +0800

    update

---
+++ webcam_detection/MVVMCommand/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace webcam_detection.MVVMCommand
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        #endregion
+
+        #region Constructor
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        #endregion
+
+        #region ICommand Members
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+        #endregion
+    }
+}
---
+++ webcam_detection/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace webcam_detection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        private VideoCapture cap = null;
+        private Mat frame;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cap = new VideoCapture(0);
+            frame = new Mat();
+            DispatcherTimer Timer = new DispatcherTimer();
+            Timer.Tick += FrameProcess;
+            Timer.Interval = TimeSpan.FromMilliseconds(30);
+            Timer.Start();
+        }
+
+        private Bitmap MatToBitmap(Mat image)
+        {
+            return BitmapConverter.ToBitmap(image);
+        }
+
+        private BitmapImage MatToBitmapImage(Mat image)
+        {
+            Bitmap bitmap = MatToBitmap(image);
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+                stream.Position = 0;
+                BitmapImage result = new BitmapImage();
+                result.BeginInit();
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.StreamSource = stream;
+                result.EndInit();
+                result.Freeze();
+                return result;
+            }
+        }
+
+        private void FrameProcess(object sender, EventArgs e)
+        {
+            if (cap.Read(frame))
+            {
+                image.Source = MatToBitmapImage(frame);
+            }
+        }
+    }
+}
